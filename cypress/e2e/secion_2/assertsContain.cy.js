885840bcabd1d743e3d6751295563a802108e4c2
Castillo <andresinho826@gmail.com>
2024-02-09 23:24:22 -0500

    agregando restante codigo assert contain

--- cypress/e2e/secion_2/assertsContain.cy.js
+++ cypress/e2e/secion_2/assertsContain.cy.js
@@ -6,6 +6,12 @@ describe("", () => {
       cy.title('eq','ToolsQA')
       cy.wait(1000)
 
+      cy.get("#userName").should("be.visible").type("andres")
+      cy.get("#userEmail").should("be.visible").type("andresinho826@gmail.com")
+      cy.get("#submit").should("be.visible").click()
+
+      cy.get("#name").should("have.text", "andres")
+
 
     })
   })

254d517b8b1e7bd31a3ee6c6be1fcae9257de94e
Castillo <andresinho826@gmail.com>
2024-02-13 01:10:56 -0500

    agregando nuevos codigo assert y nuevo file ucc email

--- cypress/e2e/secion_2/assertsContain.cy.js
+++ cypress/e2e/secion_2/assertsContain.cy.js
@@ -14,4 +14,30 @@ describe("", () => {
 
 
     })
+
+    it.only("Assert have.text mas then", () => {
+      cy.visit("https://demoqa.com/text-box")
+      cy.title('eq','ToolsQA')
+      cy.wait(1000)
+
+      
+      cy.get("#userName").should("be.visible").type("andres")
+
+      cy.get("#userName").should("contain.value", "andres").then(()=>{
+        cy.get("#userEmail").should("be.visible").type("andresinho826@gmail.com")
+        cy.get("#submit").should("be.visible").click()
+      })
+
+      /*cy.get("#userName").should("have.value", "andres").then(()=>{
+        cy.get("#userEmail").should("be.visible").type("andresinho826@gmail.com")
+        cy.get("#submit").should("be.visible").click()
+        
+        contain = este te acepta el valor a validar con mas campos example = andresssss
+        have= te obliga a que los campos a comparar sean necesariamente iguales example = andres*/
+      
+
+
+    })
+
+
   })
---
+++ cypress/e2e/secion_a/ucc.cy.js
@@ -0,0 +1,10 @@
+describe("", ()=>{
+    it("Correo UCC", ()=>{
+        cy.visit("https://outlook.office.com/")
+        cy.title("eq", "Outlook")
+        cy.wait(1000)
+
+        
+
+    })
+})

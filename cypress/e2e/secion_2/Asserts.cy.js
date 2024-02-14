7a5c6abb6d9ea6f3f16768cda322b8bff15e4f6f
Castillo <andresinho826@gmail.com>
2024-02-13 21:23:01 -0500

    agregando mas assert con css contains y otros

--- cypress/e2e/secion_2/Asserts.cy.js
+++ cypress/e2e/secion_2/Asserts.cy.js
@@ -66,9 +66,38 @@ describe("", () => {
       //cy.get("#submit").should("be.visible").click()
 
       //cy.get("#name").should("have.text", "andres")
+      
 
     })
 
+    it("assert length y el css", ()=> {
+
+      // este ejercion nos ayuda para validar el tamaño de una tabla
+      cy.visit("https://www.seleniumeasy.com/test/table-pagination-demo.html")
+      cy.title().should('eq', 'Selenium Easy - Table with pagination Demo')
+      cy.wait(1000)
+
+      cy.get("#myTable >tr >td").should("have.length", 91).and("have.css", "padding", "8px")
+    })
+
+    it("COntains", ()=> {
+      let tiempo=1000
+      // con este ejercicio lo que hacemos es omitir ventanas de emergencia o pop up
+
+      cy.visit("https://www.seleniumeasy.com/test/basic-first-form-demo.html")
+      cy.title().should('eq', 'Selenium Easy - Simple Form to automate using Selenium')
+      cy.wait(tiempo)
+
+      //eliminando la ventana emergente
+      cy.get(".at-cm-no-button").should("be.visible").click({force:true})
+
+      cy.get(".form-group > #user-message").should("be.visible").type("Demo del contenido")
+
+      //utilizamos el contain
+      cy.contains("[type='button']", "Show Message").should("be.visible").click({force:true})
+
+      
+    })
 
 
 //lastOne
